749e34f1fab51388d8d5dd7778f8a901ce1fe7bc
Daniel Rogers <danielbrogers594@gmail.com>
2020-05-21 20:27:28 -0400

    finished studio

---
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  counter(i:number){
+    return new Array(i);
+  }
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+  
+}
